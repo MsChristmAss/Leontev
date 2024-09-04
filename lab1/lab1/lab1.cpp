5cc05f6f4175ed52f8306ad16aa9ab2ec90569ce
MsChristmAss <gl1901@yandex.ru>
2024-09-04 09:42:27 +0300

    Вывод на консоль строки "Hello world", а также запрос ввода символа

--- lab1/lab1/lab1.cpp
+++ lab1/lab1/lab1.cpp
@@ -1,7 +1,11 @@
-﻿
-#include <iostream>
-
+﻿#include <iostream>
+using namespace std;
 int main()
 {
+	char a;
+	cout << "Hello World!!!" << endl;
+	cout << "Enter a symbol: ";
+	cin >> a;
+	cout << "Symbol: " << a << endl;
 }
 
